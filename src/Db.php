0c895c48a52ba5be510a92c2f0e58f3f9a8785d0
易航网络 <xh118039@qq.com>
2022-12-19 10:03:10 +0800

    111

--- src/Db.php
+++ src/Db.php
@@ -12,20 +12,42 @@ namespace JsonDb\JsonDb;
 
 class Db
 {
-    static private $options = [];
-    static public function setConfig($options)
-    {
-        self::$options = $options;
-    }
-    static public function table($table_name)
-    {
-        return (new JsonDb(self::$options))->table($table_name);
-    }
-    static public function name($table_name)
-    {
-        if (self::$options['prefix']) {
-            $table_name = self::$options['prefix'] . $table_name;
-        }
-        return (new JsonDb(self::$options))->table($table_name);
-    }
+
+	static private $options = [];
+
+	/**
+	 * 初始化配置参数
+	 * @access public
+	 * @param array $options 连接配置
+	 * @return void
+	 */
+	static public function setConfig($options)
+	{
+		self::$options = $options;
+	}
+
+	/**
+	 * 指定当前操作的数据表(不带前缀)
+	 * @access public
+	 * @param string $table 表名
+	 * @return JsonDb
+	 */
+	static public function table($table_name)
+	{
+		return (new JsonDb(self::$options))->table($table_name);
+	}
+
+	/**
+	 * 指定当前操作的数据表
+	 * @access public
+	 * @param string $table 表名
+	 * @return JsonDb
+	 */
+	static public function name($table_name)
+	{
+		if (self::$options['prefix']) {
+			$table_name = self::$options['prefix'] . $table_name;
+		}
+		return (new JsonDb(self::$options))->table($table_name);
+	}
 }
